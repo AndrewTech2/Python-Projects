8f9490085622a62ff30fec84f56b32958b956656
Andrei Ivan <iamandrewtech@gmail.com>
2025-01-28 21:56:21 +0200

    Update Automated Login

--- Automated Login/main.py
+++ Automated Login/main.py
@@ -1,16 +1,23 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import time
 
 options = Options()
 options.add_experimental_option('detach', True)
 
-driver = Chrome(options=options)
-driver.maximize_window()
-driver.get("https://the-internet.herokuapp.com/login")
-username = driver.find_element(By.NAME, 'username')
-username.send_keys('tomsmith')
+driver = Chrome(options)
+driver.implicitly_wait(5)
+
+driver.get("https://automated.pythonanywhere.com/login/")
+
+user = driver.find_element(By.NAME, 'username')
+user.send_keys("automated")
+
 password = driver.find_element(By.NAME, 'password')
-password.send_keys('SuperSecretPassword!')
-login = driver.find_element(By.TAG_NAME, 'button')
-login.click()
+password.send_keys('automatedautomated')
+
+driver.get("https://automated.pythonanywhere.com")
+time.sleep(2)
+temp = driver.find_element(By.ID, 'displaytimer')
+print(f"Average temperature: {temp.text.split(": ")[1]}")
